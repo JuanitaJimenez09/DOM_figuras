4abc69303d04adfd8a13a75024d13f54a22fdb15
Juanita Jimenez <juanitatolosa94@gmail.com>
2025-09-03 16:38:56 -0500

    funcion de la estrella

--- js/main.js
+++ js/main.js
@@ -2,13 +2,21 @@ const figure = document.getElementById('figure');
 
 function circulo(){
     figure.style.borderRadius = '50%';
-    figure.style.background = "red";
+    figure.style.background = "pink";
     
+}
+function estrella() {
+    figure.style.borderRadius = "0";
+    figure.style.background= "pink"
+    figure.style.clipPath =
+    "polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)";
+
 }
 
 
 document.addEventListener('DOMContentLoaded', function(){
     document.getElementById('btnCircle').addEventListener('click', circulo);
+    document.getElementById('btnStar').addEventListener('click', estrella )
 
 })
 
